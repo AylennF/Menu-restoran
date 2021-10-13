dd8dd40334bc29751b9d772fbb55966b94abb0cb
Aylénn Fripp <aylenn2001@gmail.com>
2021-10-13 12:07:51 -0300

    funcionamiento de las flechas

---
+++ eventosBotones.js
@@ -0,0 +1,87 @@
+let paginaEntradas = document.getElementById("paginaEntradas");
+let paginaCarnes = document.getElementById("paginaCarnes");
+let paginaPizzas = document.getElementById("paginaPizzas");
+let paginaSandwiches = document.getElementById("paginaSandwiches");
+let paginaGuarniciones = document.getElementById("paginaGuarniciones");
+let paginaPastas = document.getElementById("paginaPastas");
+let paginaBebidas = document.getElementById("paginaBebidas");
+let paginaPostres = document.getElementById("paginaPostres");
+
+let paginaEntradasIndex = document.getElementById("paginaEntradasIndex");
+let paginaCarnesIndex = document.getElementById("paginaCarnesIndex");
+let paginaPizzasIndex = document.getElementById("paginaPizzasIndex");
+let paginaSandwichesIndex = document.getElementById("paginaSandwichesIndex");
+let paginaGuarnicionesIndex = document.getElementById("paginaGuarnicionesIndex");
+let paginaPastasIndex = document.getElementById("paginaPastasIndex");
+let paginaBebidasIndex = document.getElementById("paginaBebidasIndex");
+let paginaPostresIndex = document.getElementById("paginaPostresIndex");
+
+
+paginaEntradas.onclick = () => {
+    localStorage.setItem('Pagina', 1);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaCarnes.onclick = () => {
+    localStorage.setItem('Pagina', 2);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaPizzas.onclick = () => {
+    localStorage.setItem('Pagina', 3);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaSandwiches.onclick = () => {
+    localStorage.setItem('Pagina', 4);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaGuarniciones.onclick = () => {
+    localStorage.setItem('Pagina', 5);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaPastas.onclick = () => {
+    localStorage.setItem('Pagina', 6);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaBebidas.onclick = () => {
+    localStorage.setItem('Pagina', 7);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaPostres.onclick = () => {
+    localStorage.setItem('Pagina', 8);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+
+paginaEntradasIndex.onclick = () => {
+    localStorage.setItem('Pagina', 1);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaCarnesIndex.onclick = () => {
+    localStorage.setItem('Pagina', 2);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaPizzasIndex.onclick = () => {
+    localStorage.setItem('Pagina', 3);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaSandwichesIndex.onclick = () => {
+    localStorage.setItem('Pagina', 4);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaGuarnicionesIndex.onclick = () => {
+    localStorage.setItem('Pagina', 5);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaPastasIndex.onclick = () => {
+    localStorage.setItem('Pagina', 6);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaBebidasIndex.onclick = () => {
+    localStorage.setItem('Pagina', 7);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+paginaPostresIndex.onclick = () => {
+    localStorage.setItem('Pagina', 8);
+    console.log(Number(localStorage.getItem("Pagina")));
+}
+
+
+
---
+++ scripts.js
@@ -0,0 +1,55 @@
+let flechaIzquierda = document.getElementById("flechaIzquierda");
+let flechaDerecha = document.getElementById("flechaDerecha");
+var nroPagina;
+
+window.onload = function(){
+    nroPagina=Number(localStorage.getItem("Pagina"));
+    if (nroPagina == 1){
+        $("#flechaIzquierda").hide();
+    }
+
+    if (nroPagina ===8){
+        $("#flechaDerecha").hide();
+    }
+}
+
+flechaIzquierda.onclick = () => {
+        nroPagina=nroPagina-1;
+        localStorage.setItem('Pagina', nroPagina);
+        redireccionar();
+}
+
+flechaDerecha.onclick = () => {
+        nroPagina=nroPagina+1;
+        localStorage.setItem('Pagina', nroPagina);
+        redireccionar();
+}
+
+function redireccionar(){
+    switch (nroPagina){
+        case 1:
+            location.href = "entradas.html";
+        break;
+        case 2:
+            location.href = "carnes.html";
+        break;
+        case 3:
+            location.href = "pizzas.html";
+        break;
+        case 4:
+            location.href = "sandwiches.html";
+        break;
+        case 5:
+            location.href = "guarniciones.html";
+        break;
+        case 6:
+            location.href = "pastas.html";
+        break;
+        case 7:
+            location.href = "bebidas.html";
+        break;
+        case 8:
+            location.href = "postres.html";
+        break;
+    }
+}
